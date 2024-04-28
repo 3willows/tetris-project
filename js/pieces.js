718f54eb211ab1305de195e31b70ee3959236e19
Jasper Wong <wlcjasper@gmail.com>
2024-04-28 23:42:29 +0100

    Separate the classes to pieces.js

---
+++ js/pieces.js
@@ -0,0 +1,123 @@
+// Piece classes
+
+import {width, npg2x2Cells, npg3x3Cells, npg4x4Cells} from "./scripts-current.js"
+
+export class Tpiece {
+  constructor() {
+      this.relativePosArr = [24, 23, 25, 34]
+      this.actualPosArr = []
+      this.cssClass = 't'
+      this.rotationIdx = 0
+      this.rotationOffsets = [
+          [0, 1, width, -1],
+          [0, width, -1, -width],
+          [0, -1, -width, 1],
+          [0, -width, 1, width]
+      ]
+      this.nextPieceGrid = npg3x3Cells
+      this.nextPiecePos = [0, 1, 2, 4]
+  }
+}
+
+export class Spiece {
+  constructor() {
+      this.relativePosArr = [34, 24, 25, 33]
+      this.actualPosArr = []
+      this.cssClass = 's'
+      this.rotationIdx = 0
+      this.rotationOffsets = [
+          [0, -width, -(width - 1), -1],
+          [0, 1, (width + 1), -width],
+          [0, width, (width - 1), 1],
+          [0, -1, -(width + 1), width]
+      ]
+      this.nextPieceGrid = npg3x3Cells
+      this.nextPiecePos = [1, 2, 3, 4]
+  }
+}
+
+export class Zpiece {
+  constructor() {
+      this.relativePosArr = [34, 24, 23, 35]
+      this.actualPosArr = []
+      this.cssClass = 'z'
+      this.rotationIdx = 0
+      this.rotationOffsets = [
+          [0, -width, -(width + 1), 1],
+          [0, 1, -(width - 1), width],
+          [0, width, -1, (width + 1)],
+          [0, -1, -width, (width -1)]
+      ]
+      this.nextPieceGrid = npg3x3Cells
+      this.nextPiecePos = [0, 1, 4, 5]
+  }
+}
+
+export class Lpiece {
+  constructor() {
+      this.relativePosArr = [24, 14, 35, 34]
+      this.actualPosArr = []
+      this.cssClass = 'l'
+      this.rotationIdx = 0
+      this.rotationOffsets = [
+          [0, -width, (width + 1), width],
+          [0, 1, (width - 1), -1],
+          [0, width, -(width + 1), -width],
+          [0, -1, -(width - 1), 1]
+      ]
+      this.nextPieceGrid = npg3x3Cells
+      this.nextPiecePos = [2, 3, 4, 5]
+  }
+}
+
+export class Jpiece {
+  constructor() {
+      this.relativePosArr = [24, 14, 34, 33]
+      this.actualPosArr = []
+      this.cssClass = 'j'
+      this.rotationIdx = 0
+      this.rotationOffsets = [
+          [0, -width, width, (width - 1)],
+          [0, 1, -1, -(width + 1)],
+          [0, width, -width, -(width - 1)],
+          [0, -1, 1, (width + 1)]
+      ]
+      this.nextPieceGrid = npg3x3Cells
+      this.nextPiecePos = [0, 1, 2, 5]
+  }
+}
+
+export class Opiece {
+  constructor() {
+      this.relativePosArr = [34, 24, 25, 35]
+      this.actualPosArr = []
+      this.cssClass = 'o'
+      this.rotationIdx = 0
+      this.rotationOffsets = [
+          [0, -width, -(width - 1), 1],
+          [0, -width, -(width - 1), 1],
+          [0, -width, -(width - 1), 1],
+          [0, -width, -(width - 1), 1]
+      ]
+      this.nextPieceGrid = npg2x2Cells
+      this.nextPiecePos = [0, 1, 2, 3]
+  }
+}
+
+export class Ipiece {
+  constructor() {
+      this.relativePosArr = [35, 36, 33, 34]
+      this.actualPosArr = []
+      this.cssClass = 'i'
+      this.rotationIdx = 0
+      this.rotationOffsets = [
+          [0, 1, -2, -1],
+          [0, width, -(width * 2), -width],
+          [0, -1, 2, 1],
+          [0, -width, (width * 2), width]
+      ]
+      this.nextPieceGrid = npg4x4Cells
+      this.nextPiecePos = [4, 5, 6, 7]
+  }
+}
+
--- js/scripts-current.js
+++ js/scripts-current.js
@@ -1,3 +1,5 @@
+import {Tpiece, Spiece, Zpiece, Lpiece, Jpiece, Opiece, Ipiece} from "./pieces.js"
+
 // ! Variables & Elements
 // ? DOM Elements
 const board = document.querySelector('.board')
@@ -24,137 +26,19 @@ const rotateSound = document.getElementById('rotate')
 
 // ? Variables
 // Board config
-const width = 10
-const height = 24
+export const width = 10
+export const height = 24
 const hiddenRows = 4
 const cellCount = width * height
 let cells = []
 
 // Next piece grid config
-let npg3x3Cells = nextPieceGrid(3, 3)
-let npg2x2Cells = nextPieceGrid(2, 2)
-let npg4x4Cells = nextPieceGrid(4, 4)
+export let npg3x3Cells = nextPieceGrid(3, 3)
+export let npg2x2Cells = nextPieceGrid(2, 2)
+export let npg4x4Cells = nextPieceGrid(4, 4)
 const npgArr = [npg3x3Cells, npg2x2Cells, npg4x4Cells]
 
 // Piece classes
-class Tpiece {
-    constructor() {
-        this.relativePosArr = [24, 23, 25, 34]
-        this.actualPosArr = []
-        this.cssClass = 't'
-        this.rotationIdx = 0
-        this.rotationOffsets = [
-            [0, 1, width, -1],
-            [0, width, -1, -width],
-            [0, -1, -width, 1],
-            [0, -width, 1, width]
-        ]
-        this.nextPieceGrid = npg3x3Cells
-        this.nextPiecePos = [0, 1, 2, 4]
-    }
-}
-
-class Spiece {
-    constructor() {
-        this.relativePosArr = [34, 24, 25, 33]
-        this.actualPosArr = []
-        this.cssClass = 's'
-        this.rotationIdx = 0
-        this.rotationOffsets = [
-            [0, -width, -(width - 1), -1],
-            [0, 1, (width + 1), -width],
-            [0, width, (width - 1), 1],
-            [0, -1, -(width + 1), width]
-        ]
-        this.nextPieceGrid = npg3x3Cells
-        this.nextPiecePos = [1, 2, 3, 4]
-    }
-}
-
-class Zpiece {
-    constructor() {
-        this.relativePosArr = [34, 24, 23, 35]
-        this.actualPosArr = []
-        this.cssClass = 'z'
-        this.rotationIdx = 0
-        this.rotationOffsets = [
-            [0, -width, -(width + 1), 1],
-            [0, 1, -(width - 1), width],
-            [0, width, -1, (width + 1)],
-            [0, -1, -width, (width -1)]
-        ]
-        this.nextPieceGrid = npg3x3Cells
-        this.nextPiecePos = [0, 1, 4, 5]
-    }
-}
-
-class Lpiece {
-    constructor() {
-        this.relativePosArr = [24, 14, 35, 34]
-        this.actualPosArr = []
-        this.cssClass = 'l'
-        this.rotationIdx = 0
-        this.rotationOffsets = [
-            [0, -width, (width + 1), width],
-            [0, 1, (width - 1), -1],
-            [0, width, -(width + 1), -width],
-            [0, -1, -(width - 1), 1]
-        ]
-        this.nextPieceGrid = npg3x3Cells
-        this.nextPiecePos = [2, 3, 4, 5]
-    }
-}
-
-class Jpiece {
-    constructor() {
-        this.relativePosArr = [24, 14, 34, 33]
-        this.actualPosArr = []
-        this.cssClass = 'j'
-        this.rotationIdx = 0
-        this.rotationOffsets = [
-            [0, -width, width, (width - 1)],
-            [0, 1, -1, -(width + 1)],
-            [0, width, -width, -(width - 1)],
-            [0, -1, 1, (width + 1)]
-        ]
-        this.nextPieceGrid = npg3x3Cells
-        this.nextPiecePos = [0, 1, 2, 5]
-    }
-}
-
-class Opiece {
-    constructor() {
-        this.relativePosArr = [34, 24, 25, 35]
-        this.actualPosArr = []
-        this.cssClass = 'o'
-        this.rotationIdx = 0
-        this.rotationOffsets = [
-            [0, -width, -(width - 1), 1],
-            [0, -width, -(width - 1), 1],
-            [0, -width, -(width - 1), 1],
-            [0, -width, -(width - 1), 1]
-        ]
-        this.nextPieceGrid = npg2x2Cells
-        this.nextPiecePos = [0, 1, 2, 3]
-    }
-}
-
-class Ipiece {
-    constructor() {
-        this.relativePosArr = [35, 36, 33, 34]
-        this.actualPosArr = []
-        this.cssClass = 'i'
-        this.rotationIdx = 0
-        this.rotationOffsets = [
-            [0, 1, -2, -1],
-            [0, width, -(width * 2), -width],
-            [0, -1, 2, 1],
-            [0, -width, (width * 2), width]
-        ]
-        this.nextPieceGrid = npg4x4Cells
-        this.nextPiecePos = [4, 5, 6, 7]
-    }
-}
 
 const pieceClasses = [Tpiece, Spiece, Zpiece, Lpiece, Jpiece, Opiece, Ipiece]
 
